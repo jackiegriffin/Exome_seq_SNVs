60b89dc55080db0640746664869c7ab7ece787fd
lilfijj <jacqueline.r.griffin@dartmouth.edu>
2020-10-15 14:34:27 -0400

    R club

--- recurrent_variant calls.R
+++ recurrent_variant calls.R
@@ -15,10 +15,22 @@ filter_out_synonymous <- function(file_name) {
 # Upload data ----
 variant_calls <- list.files(path = "C:/Users/jackie/OneDrive - Dartmouth College/Exome-mutation-calls/Input_variant_call_files/", pattern = "\\.txt$")                          # create chr vector of all file names with .txt extension
 variant_calls <- paste('Input_variant_call_files/', variant_calls, sep = '')  # paste folder name (containing .txt files) to reflect PATH from wd
+variant_calls
+
+base_49_no_filter <- read.delim(file="Input_variant_call_files/twm_17_049_variants_filtered.txt")
+
 
 #     1. Run 'filter_out_synonymous' fxn on 'variant_calls' files
 #     2. Convert 'hgvs_c' and 'hgvs_p' to characters vectors
 #     3. Write filtered .csv files
+
+
+# want effects: initiator_codon, missense, noncoding, protein-protein, 
+# start codon loss or gain, stop codon-loss or gain, structural changes. 
+
+# only keep rows with exact 'effect'  name
+hist(base_49$AF)
+
 
 base_49 <- filter_out_synonymous(variant_calls[1])
 base_49$hgvs_c <- as.character(base_49$hgvs_c)                                     
@@ -60,7 +72,8 @@ genes_in_all_5_recur <- Reduce(intersect, list(recur_293$hgvs_c,
                                                      recur_346$hgvs_c,
                                                      recur_347$hgvs_c,
                                                      recur_348$hgvs_c))
-#base_49_short <- base_49[base_49$hgvs_c %in% genes_in_all_3, ]
+
+base_49_short <- base_49[base_49$hgvs_c %in% genes_in_all_3, ]
 
 
 # Venn diagram ----
@@ -88,7 +101,9 @@ genes_in_all_3_base <- as.data.frame(genes_in_all_3_base)
 colnames(genes_in_all_3_base) <- c("hgvs_c")
 genes_in_all_5_recur <- as.data.frame(genes_in_all_5_recur)
 colnames(genes_in_all_5_recur) <- c("hgvs_c")
+
 SNVs_unique_to_reccurent <- anti_join (genes_in_all_5_recur, genes_in_all_3_base, by='hgvs_c')  
+
 write.csv(SNVs_unique_to_reccurent, file = "SNVs_unique_to_reccurent.csv")
 
 
